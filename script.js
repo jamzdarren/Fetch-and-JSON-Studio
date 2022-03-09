369ea725e4bfcec60c65c05199e913861554e8cc
James <jamzdarren@gmail.com>
2022-03-09 15:34:56 -0600

    Added json call and formatted response

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {
+            json.sort((a,b) => (a.hoursInSpace > b.hoursInSpace) ? -1 : 1);
+            const destination = document.getElementById("container");
+            for (let i = 0; i < json.length; i++) {
+
+                let active = "";
+                if (json[i].active === true) {
+                    active = ' class="active"';
+                }
+
+                destination.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li${active}>Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${json[i].picture}">
+                    </div>
+                `;
+            }
+            destination.innerHTML += `Astronaut Count: ${json.length}`
+        });
+    });
+ });
